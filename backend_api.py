5d144a41c493bc9f343277f80d7b394aabbb8fe5
hezron-sketch <hpaulhezne@gmail.com>
2025-07-17 03:11:30 +0300

    fixing ports

--- backend_api.py
+++ backend_api.py
@@ -175,6 +175,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "backend_api:app",
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
+        port=int(os.environ.get("PORT", 10000)),
         reload=True if os.environ.get("DEV") == "1" else False
     )
